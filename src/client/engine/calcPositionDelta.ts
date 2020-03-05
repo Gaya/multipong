241df39f339bea9d7cfbb9069cc9f86810b21cab
Gaya Kessler <gaya@theclevernode.com>
2020-03-05 19:55:16 +0100

    implement position delta

--- src/client/engine/calcPositionDelta.ts
+++ src/client/engine/calcPositionDelta.ts
@@ -4,33 +4,36 @@ interface Coordinate {
 }
 
 function calcPositionDelta(timePassed: number, speed: number, angle: number): Coordinate {
-  /*
-          C
-      b   a
-  A   c   B
-
-  a:	5
-  b: 10
-  c: 8.66
-  Angle A (α):	30
-  Angle B (β):	90
-  Angle C (γ):	60
-
-  Calc a:
-  Math.tan(30 * (Math.PI / 180)) * 10
-
-  Calc c:
-  Math.tan(30 * (Math.PI / 180)) * 10
-
- */
-
-  const distanceTraveled = ((timePassed / 15) * speed);
   const calcAngle = angle % 90;
+  const c = ((timePassed / 15) * speed);
+  const a = c * Math.sin(calcAngle * (Math.PI / 180));
+  const b = Math.sqrt(c ** 2 - a ** 2);
 
-  return {
-    x: 0,
-    y: 0,
-  };
+  const axis = Math.ceil((angle + 1) / 90) % 4;
+
+  switch (axis) {
+    case 4:
+      return {
+        x: a,
+        y: b * -1 || 0,
+      };
+    case 3:
+      return {
+        x: b * -1 || 0,
+        y: a * -1 || 0,
+      };
+    case 2:
+      return {
+        x: a * -1 || 0,
+        y: b,
+      };
+    case 1:
+    default:
+      return {
+        x: b,
+        y: a,
+      };
+  }
 }
 
 export default calcPositionDelta;
